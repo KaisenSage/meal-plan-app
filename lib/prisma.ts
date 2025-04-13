9846206e300fce80ef128b5bef213abb0cc5c79a
Kaisensage <kvngbabstar@gmail.com>
2025-04-13 10:05:19 +0100

    Fix: export prisma from lib/prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,12 +5,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
-  globalForPrisma.prisma ??
+export const prisma =
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: ['query'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export const db = prisma; // ✅ THIS IS THE IMPORTANT PART
