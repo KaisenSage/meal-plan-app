2f5fa28f51f9b16930ad55905c43e9691abf5e5c
Kaisensage <kvngbabstar@gmail.com>
2025-04-14 09:34:29 +0100

    page styling 1

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Link from "next/link";
+import './Footer.css';
 import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 export default function HomePage() {
   return (
